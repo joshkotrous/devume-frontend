3e153e109f296e716b8ba5a8605df845e6ab8a4e
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-24 00:20:15 -0400

    Profile page updates

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Card, CardHeader, CardBody, Avatar } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, Avatar, Button } from "@nextui-org/react";
 import { GetProfile } from "../hooks/Profiles";
 import { useEffect, useState } from "react";
 
@@ -15,23 +15,32 @@ const UserProfile = () => {
     getProfileData();
   }, []);
   return (
-    <div className="h-[90vh] max-w-[1024px] flex justify-center">
-      <Card className="py-4">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <Avatar className="mb-2 self-center" />
-          {profileData.user && profileData.profile ? (
-            <>
-              <p>
-                {profileData.user.first_name + " " + profileData.user.last_name}
-              </p>
-              <h2>{username}</h2>
-              <p>{profileData.profile.bio}</p>
-              <p>{profileData.profile.birth_date}</p>
-            </>
-          ) : null}
-        </CardHeader>
-        <CardBody className="overflow-visible py-2"></CardBody>
-      </Card>
+    <div className="h-[90vh] flex justify-center">
+      <div className="h-full w-full flex max-w-[1024px]">
+        <Card className="h-fit w-1/3 mr-4 py-4">
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <Avatar className="mb-2 self-center" />
+            <Button className="mb-2 self-center">Edit Profile</Button>
+            {profileData.user && profileData.profile ? (
+              <>
+                <p>
+                  {profileData.user.first_name +
+                    " " +
+                    profileData.user.last_name}
+                </p>
+                <h2>{username}</h2>
+                <p>{profileData.profile.bio}</p>
+                <p>{profileData.profile.birth_date}</p>
+              </>
+            ) : null}
+          </CardHeader>
+          <CardBody className="overflow-visible py-2"></CardBody>
+        </Card>
+        <Card className="h-full w-full py-4">
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start"></CardHeader>
+          <CardBody className="overflow-visible py-2"></CardBody>
+        </Card>
+      </div>
     </div>
   );
 };
